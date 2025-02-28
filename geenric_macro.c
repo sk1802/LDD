966d2053b835e9dd8a3837a2ed51c28a1c44953e
sk1802 <88622678+sk1802@users.noreply.github.com>
2025-02-28 23:11:28 +0530

    Update geenric_macro.c
    for a more generic approach

--- geenric_macro.c
+++ geenric_macro.c
@@ -20,3 +20,26 @@
     }                                                               \
     __ret;                                                          \
 })
+
+#define OF_PROPERTY_READ(np, propname, out, type) ({                   \
+    int __ret = -EINVAL;                                               \
+    if ((type) == u32)                                                 \
+        __ret = of_property_read_u32((np), (propname), (out));         \
+    else if ((type) == u16)                                            \
+        __ret = of_property_read_u16((np), (propname), (out));         \
+    else if ((type) == bool) {                                         \
+        *(out) = of_property_read_bool((np), (propname));              \
+        __ret = 0;                                                     \
+    }                                                                  \
+    if (__ret == 0) {                                                  \
+        if ((type) == bool)                                            \
+            pr_info("Device Tree: Found property '%s', value: %s\n",   \
+                    (propname), *(out) ? "true" : "false");            \
+        else                                                           \
+            pr_info("Device Tree: Found property '%s', value: %u\n",   \
+                    (propname), (unsigned int)(*(out)));               \
+    } else {                                                           \
+        pr_err("Device Tree: Property '%s' not found\n", (propname));  \
+    }                                                                  \
+    __ret;                                                             \
+})
